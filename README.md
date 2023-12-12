0b2289dda154ae08431e6c29a14c1de5a9615912
N7ghtm4r3 <maurizio.manuel2003@gmail.com>
2023-12-12 13:47:05 +0100

    Documented the library

--- README.md
+++ README.md
@@ -1,2 +1,78 @@
 # Mantis
 Manage the translation of the content of your applications with Mantis!
+
+### Usage/Examples
+
+#### Resources file
+
+```json
+{
+    // current locale
+    "it-IT": {
+      "string_one" : "ciao!",
+      "string_two": "prova questa libreria :)"
+    },
+    // all translations
+    "en-EN": {
+      "string_one" : "hello!", 
+      "string_two": "try this library :)"
+    }
+//  ... 
+}
+```
+
+#### Workflow
+
+```java
+// current locale en-EN
+Mantis mantis = new Mantis(Locale.getDefault());
+System.out.println(mantis.getResource("string_one")); // hello!
+
+// current locale it-IT
+Mantis mantis = new Mantis(Locale.getDefault());
+System.out.println(mantis.getResource("string_one")); // ciao!
+
+// Dynamic translation of any contents
+System.out.println(mantis.translate("Hello how are you?")); // Ciao, come stai?
+```
+
+## Authors
+
+- [@N7ghtm4r3](https://www.github.com/N7ghtm4r3)
+
+## Support
+
+If you need help using the library or encounter any problems or bugs, please contact us via the following links:
+
+- Support via <a href="mailto:infotecknobitcompany@gmail.com">email</a>
+- Support via <a href="https://github.com/N7ghtm4r3/Mantis/issues/new">GitHub</a>
+
+Thank you for your help!
+
+## Badges
+
+[![](https://img.shields.io/badge/Google_Play-414141?style=for-the-badge&logo=google-play&logoColor=white)](https://play.google.com/store/apps/developer?id=Tecknobit)
+[![Twitter](https://img.shields.io/badge/Twitter-1DA1F2?style=for-the-badge&logo=twitter&logoColor=white)](https://twitter.com/tecknobit)
+
+[![](https://img.shields.io/badge/Java-ED8B00?style=for-the-badge&logo=java&logoColor=white)](https://www.oracle.com/java/)
+
+[![](https://jitpack.io/v/N7ghtm4r3/Mantis.svg)](https://jitpack.io/#N7ghtm4r3/Mantis)
+
+## Business contact
+
+If you need to contact me for a project
+
+[![](https://img.shields.io/badge/fiverr-1DBF73?style=for-the-badge&logo=fiverr&logoColor=white)](https://www.fiverr.com/manuel_maurizio)
+
+## Donations
+
+If you want support project and developer
+
+| Crypto                                                                                              | Address                                        | Network  |
+|-----------------------------------------------------------------------------------------------------|------------------------------------------------|----------|
+| ![](https://img.shields.io/badge/Bitcoin-000000?style=for-the-badge&logo=bitcoin&logoColor=white)   | **3H3jyCzcRmnxroHthuXh22GXXSmizin2yp**         | Bitcoin  |
+| ![](https://img.shields.io/badge/Ethereum-3C3C3D?style=for-the-badge&logo=Ethereum&logoColor=white) | **0x1b45bc41efeb3ed655b078f95086f25fc83345c4** | Ethereum |
+
+If you want support project and developer with <a href="https://www.paypal.com/donate/?hosted_button_id=5QMN5UQH7LDT4">PayPal</a>
+
+Copyright © 2023 Tecknobit
--- src/main/java/Mantis.java
+++ src/main/java/Mantis.java
@@ -8,18 +8,61 @@ import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.util.Locale;
 
+/**
+ * The {@code Mantis} class is useful to translate all the {@link String} resources in different languages, e.g
+ * <pre>
+ *     {@code
+ * {
+ *     // current locale
+ *    "it-IT": {
+ *         "string_one" : "ciao!",
+ *         "string_two": "prova questa libreria :)"
+ *     },
+ *     // all translations
+ *     "en-EN": {
+ *         "string_one" : "hello!",
+ *         "string_two": "try this library :)"
+ *     }
+ *     ...
+ * }
+ *
+ *     }
+ * </pre>
+ * @author N7ghtm4r3 - Tecknobit
+ */
 public class Mantis {
 
+    /**
+     * {@code MANTIS_RESOURCES_PATH} the path where are stored the resources to translate
+     */
     private static final String MANTIS_RESOURCES_PATH = "resources.mantis";
 
+    /**
+     * {@code currentLocale} the current locale language chosen as default
+     */
     private Locale currentLocale;
 
+    /**
+     * {@code resources} the resources data
+     */
     private JsonHelper resources;
 
+    /**
+     * Constructor to init the {@link Mantis} object
+     *
+     * @param currentLocale:{@code currentLocale} the current locale language chosen as default
+     *
+     */
     public Mantis(String currentLocale) throws IOException {
         this(Locale.forLanguageTag(currentLocale));
     }
 
+    /**
+     * Constructor to init the {@link Mantis} object
+     *
+     * @param currentLocale:{@code currentLocale} the current locale language chosen as default
+     *
+     */
     public Mantis(Locale currentLocale) throws IOException {
         try {
             setCurrentResources();
@@ -29,6 +72,12 @@ public class Mantis {
         this.currentLocale = currentLocale;
     }
 
+    /**
+     * Method to read the current resources stored from {@link #MANTIS_RESOURCES_PATH} and to load the {@link #resources}
+     * instance <br>
+     * No-any params required
+     * @throws IOException when an error reading the resources file occurred
+     */
     private void setCurrentResources() throws IOException {
         InputStream inputStream = getClass().getClassLoader().getResourceAsStream(MANTIS_RESOURCES_PATH);
         if(inputStream == null)
@@ -41,51 +90,68 @@ public class Mantis {
         resources = new JsonHelper(stringBuilder.toString());
     }
 
+    /**
+     * Method to get a resource from the {@link #MANTIS_RESOURCES_PATH} resources file in the {@link #currentLocale} chosen
+     * @param resourceKey the key of the resource to get e.g. "string_one"
+     * @return the resource in the {@link #currentLocale} chosen as {@link String}
+     */
     public String getResource(String resourceKey) {
         return resources.getJsonHelper(currentLocale.toLanguageTag()).getString(resourceKey, "");
     }
 
+    /**
+     * Method to change the {@link Locale} for the {@link #currentLocale} chosen
+     * @param newLocale: the new {@link Locale} to use
+     */
     public void changeCurrentLocale(String newLocale) {
         changeCurrentLocale(Locale.forLanguageTag(newLocale));
     }
 
+    /**
+     * Method to change the {@link Locale} for the {@link #currentLocale} chosen
+     * @param newLocale: the new {@link Locale} to use
+     */
     public void changeCurrentLocale(Locale newLocale) {
         this.currentLocale = newLocale;
     }
 
     /**
-     * RUSSIAN
-     * ENGLISH
-     * ARABIC
-     * AZERBAIJANI
-     * CATALAN
-     * CHINESE
-     * CZECH
-     * DANISH
-     * DUTCH
-     * ESPERANTO
-     * FINNISH
-     * FRENCH
-     * GERMAN
-     * GREEK
-     * HEBREW
-     * HINDI
-     * HUNGARIAN
-     * INDONESIAN
-     * IRISH
-     * ITALIAN
-     * JAPANESE
-     * KOREAN
-     * PERSIAN
-     * POLISH
-     * PORTUGUESE
-     * SLOVAK
-     * SPANISH
-     * SWEDISH
-     * TURKISH
+     * Method to translate a dynamic content
+     * @param content: the content to translate
+     * @return the content translated as {@link String}
+     * @apiNote the translation is executed by the <a href="https://github.com/dynomake/libretranslate-java">
+     *     libretranslate-java</a> library
+     * @implNote current languages available:
+     * RUSSIAN,
+     * ENGLISH,
+     * ARABIC,
+     * AZERBAIJANI,
+     * CATALAN,
+     * CHINESE,
+     * CZECH,
+     * DANISH,
+     * DUTCH,
+     * ESPERANTO,
+     * FINNISH,
+     * FRENCH,
+     * GERMAN,
+     * GREEK,
+     * HEBREW,
+     * HINDI,
+     * HUNGARIAN,
+     * INDONESIAN,
+     * IRISH,
+     * ITALIAN,
+     * JAPANESE,
+     * KOREAN,
+     * PERSIAN,
+     * POLISH,
+     * PORTUGUESE,
+     * SLOVAK,
+     * SPANISH,
+     * SWEDISH,
+     * TURKISH,
      * UKRAINIAN
-     * @param content
-     * @return
      */
     public String translate(String content) {
         Language language = null;
